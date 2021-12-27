dac5335a2287fcb75fc66d50460dccfa89271524
Jung Ha Baek <junghabaek@gmail.com>
2021-12-27 20:50:06 +0900

    Revert "Be jinmook jungha - models.py update"
    This reverts commit 80b216a0529949f2ad121e85eb360adf904ae9ed.

--- back/app.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask
-from models import db
-
-app = Flask(__name__)
--- back/views/user_view.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, request, session
-from models import *
-from werkzeug.security import check_password_hash, generate_password_hash
-
-bp = Blueprint('users', __name__, url_prefix='/users')
-
-
