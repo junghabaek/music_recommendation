8e0444f4cd2458b01ec8854d174accb60c4b430b
jinmook <jink0608@naver.com>
2021-12-28 17:01:49 +0900

    merge하면서 삭제된 re모듈 다시 추가해주기

--- back/views/user_view.py
+++ back/views/user_view.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, json, request, session, jsonify
 from models import Users, db
 from werkzeug.security import check_password_hash, generate_password_hash
+import re
 
 bp = Blueprint('users', __name__, url_prefix='/users')
 
