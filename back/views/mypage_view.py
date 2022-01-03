3ec4a1caf153973074278f2854a92fdc66f4c04c
Jung Ha Baek <junghabaek@gmail.com>
2022-01-03 15:15:31 +0900

    add mypage_view

---
+++ back/views/mypage_view.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, json, request, session, jsonify
+#from models import Users, db
+
+
+bp = Blueprint('mypage', __name__, url_prefix='/mypage')
+
+#user_id는 get으로 받아온 값으로 추후에 query에 이용
+
+
+@bp.route('/<int:user_id>', methods=['GET'])
+def showData(user_id):
+
+    response = {
+        'id': 'movie_id',
+        'movie_title': '통신완료 영화제목',
+        'movie_director': '통신완료 영화감독',
+        'sound_director': '통신완료 음향감독',
+        'movie_description': '통신완료 영화 줄거리',
+        'img_url': '통신완료 이미지 주소입니다.'
+
+    }
+
+    return jsonify(response)
