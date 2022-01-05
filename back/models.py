f3bc0c0a58c115b090ed036f08fdd3bc327ffba2
Jung Ha Baek <junghabaek@gmail.com>
2022-01-05 16:09:00 +0900

    models.py update

--- back/models.py
+++ back/models.py
@@ -4,28 +4,6 @@ from datetime import datetime
 db = SQLAlchemy()
 
 
-class Users(db.Model):
-    __tablename__ = "Users"
-
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
-    email = db.Column(db.String(100), nullable=True)
-    nickname = db.Column(db.String(50), nullable=True)
-    password = db.Column(db.String(200), nullable=False)
-    Netflix = db.Column(db.Boolean, nullable=False, default=False)
-    Disney = db.Column(db.Boolean, nullable=False, default=False)
-    Prime = db.Column(db.Boolean, nullable=False, default=False)
-    Hulu = db.Column(db.Boolean, nullable=False, default=False)
-
-    def __init__(self, email, nickname, password, Netflix, Disney, Prime, Hulu):
-        self.email = email
-        self.nickname = nickname
-        self.password = password
-        self.Netflix = Netflix
-        self.Disney = Disney
-        self.Prime = Prime
-        self.Hulu = Hulu
-
 
 class Movies(db.Model):
     __tablename__ = "Movies"
@@ -109,20 +87,6 @@ class Features(db.Model):
       return self.valence
 
 
-class Favorites (db.Model):
-    __tablename__ = "Favorites"
-
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
-    movie_id = db.Column(db.Integer, db.ForeignKey(
-        'Movies.id'), nullable=False)
-    createdAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updatedAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    def __init__(self, user_id, movie_id):
-        self.user_id = user_id
-        self.movie_id = movie_id
 
 
 class Genres(db.Model):
@@ -141,35 +105,7 @@ class Genres(db.Model):
         self.genre_id = genre_id
 
 
-class User_features (db.Model):
 
-  __tablename__ = "User_features"
-
-  id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-  user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
-  movie_id = db.Column(db.Integer, db.ForeignKey('Movies.id'), nullable=False)
-  acousticness = db.Column(db.Float)
-  danceability = db.Column(db.Float)
-  energy = db.Column(db.Float)
-  tempo = db.Column(db.Float)
-  valence = db.Column(db.Float)
-  instrumentalness = db.Column(db.Float)
-  liveness = db.Column(db.Float)
-  loudness = db.Column(db.Float)
-  speechiness = db.Column(db.Float)
-
-  def __init__(self, user_id, movie_id, acousticness, danceability, energy, tempo, valence, instrumentalness, liveness, loudness, speechiness):
-    self.user_id= user_id
-    self.movie_id = movie_id
-    self.acousticness = acousticness
-    self.danceability = danceability
-    self.energy = energy
-    self.tempo = tempo
-    self.valence = valence
-    self.instrumentalness = instrumentalness
-    self.liveness = liveness
-    self.loudness = loudness
-    self.speechiness = speechiness
 
 
 # 여기는 아직은 옵셔널입니다
