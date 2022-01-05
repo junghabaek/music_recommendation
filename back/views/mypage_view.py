89a845710265c0c064bf0c692193e8666cadbb1b
Jung Ha Baek <junghabaek@gmail.com>
2022-01-05 12:00:06 +0900

    add mypage_view

--- back/views/mypage_view.py
+++ back/views/mypage_view.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, json, request, session, jsonify
-#from models import Users, db
+from models import Movies, db
 
 
 bp = Blueprint('mypage', __name__, url_prefix='/mypage')
@@ -7,16 +7,28 @@ bp = Blueprint('mypage', __name__, url_prefix='/mypage')
 #user_id는 get으로 받아온 값으로 추후에 query에 이용
 
 
-@bp.route('/<int:user_id>', methods=['GET'])
-def showData(user_id):
+@bp.route('/<int:movie_id>', methods=['GET'])
+def showData(movie_id):
+
+    movie=Movies.query.filter(Movies.id==movie_id).first()
 
     response = {
-        'id': 'movie_id',
-        'movie_title': '통신완료 영화제목',
-        'movie_director': '통신완료 영화감독',
-        'sound_director': '통신완료 음향감독',
-        'movie_description': '통신완료 영화 줄거리',
-        'img_url': '통신완료 이미지 주소입니다.'
+        'movie_id' : movie_id,
+        'movie_title' : movie.movie_title,
+        'movie_year' : movie.movie_year,
+        'movie_director' : movie.movie_director,
+        'sound_director' : movie.sound_director,
+        'imdb' : movie.imdb,
+        'Netflix' : movie.Netflix,
+        'Hulu' : movie.Hulu,
+        'Prime' : movie.Prime,
+        'Disney' : movie.Disney,
+        'country' : movie.country,
+        'Language': movie.Language,
+        'runtime' : movie.runtime,
+        'movie_age_rating' : movie.movie_age_rating,
+        'poster_url' : movie.poster_url,
+        'like_count' : movie.like_count
 
     }
 
